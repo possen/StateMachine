8b45428d2aba6026fcb8a18f4376a7be7607672b
Paul Ossenbruggen <paul.ossenbruggen@ridecell.com>
2018-10-16 17:39:44 -0700

    Initial Commit

---
+++ StateMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StateMachine
+//
+//  Created by Paul Ossenbruggen on 10/16/18.
+//  Copyright © 2018 Paul Ossenbruggen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
